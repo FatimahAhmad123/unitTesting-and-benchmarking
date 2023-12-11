88d74223bba81fe17955cf5b7a12488d04cd7a73
Fatimah <Fatimahse25@gmail.com>
2023-12-11 22:07:54 +0500

    Updated benchmark.cpp and .gitignore

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,17 +1,14 @@
-#include <benchmark/benchmark.h>
+#include "benchmark/benchmark.h"
 #include "codility.h"
 
-int iterations = 10;
 Codility codility;
 static void BM_BinaryGap_RegularCases(benchmark::State &state)
 {
 
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.binaryGap(529);
-		}
+
+		codility.binaryGap(529);
 	}
 }
 BENCHMARK(BM_BinaryGap_RegularCases);
@@ -21,10 +18,7 @@ static void BM_BinaryGap_OutOfBoundValues(benchmark::State &state)
 
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.binaryGap(-5);
-		}
+		codility.binaryGap(-5);
 	}
 }
 BENCHMARK(BM_BinaryGap_OutOfBoundValues);
@@ -34,10 +28,8 @@ static void BM_BinaryGap_LargeNumber(benchmark::State &state)
 
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.binaryGap(2147483647);
-		}
+
+		codility.binaryGap(2147483647);
 	}
 }
 BENCHMARK(BM_BinaryGap_LargeNumber);
@@ -48,10 +40,7 @@ static void BM_CircularRotation_RegularCases(benchmark::State &state)
 	std::vector<int> A = {1, 2, 3, 4, 5, 6, 7};
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.circularRotation(A, 3);
-		}
+		codility.circularRotation(A, 3);
 	}
 }
 BENCHMARK(BM_CircularRotation_RegularCases);
@@ -62,10 +51,7 @@ static void BM_CircularRotation_EmptyVector(benchmark::State &state)
 	std::vector<int> A;
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.circularRotation(A, 3);
-		}
+		codility.circularRotation(A, 3);
 	}
 }
 BENCHMARK(BM_CircularRotation_EmptyVector);
@@ -76,10 +62,7 @@ static void BM_CircularRotation_EmptyVectorLargeK(benchmark::State &state)
 	std::vector<int> A;
 	for (auto _ : state)
 	{
-		for (int i = 0; i < iterations; ++i)
-		{
-			codility.circularRotation(A, 100);
-		}
+		codility.circularRotation(A, 100);
 	}
 }
 BENCHMARK(BM_CircularRotation_EmptyVectorLargeK);
